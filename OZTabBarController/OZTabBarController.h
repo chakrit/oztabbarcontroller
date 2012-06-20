0e3b533ed2cf323cf1d05725373068405a7a563f
Chakrit Wichian <chakrit@oozou.com>
2012-06-20 16:49:17 +0700

    Initial Commit

---
+++ OZTabBarController/OZTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  OZTabBarController.h
+//  OZTabBarController
+//
+//  Created by Chakrit Wichian on 6/20/12.
+//  Copyright (c) 2012 Oozou Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OZTabBarController : NSObject
+
+@end
---
+++ OZTabBarController/OZTabBarController.m
@@ -0,0 +1,13 @@
+//
+//  OZTabBarController.m
+//  OZTabBarController
+//
+//  Created by Chakrit Wichian on 6/20/12.
+//  Copyright (c) 2012 Oozou Ltd. All rights reserved.
+//
+
+#import "OZTabBarController.h"
+
+@implementation OZTabBarController
+
+@end
