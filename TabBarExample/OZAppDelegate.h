784ae6f640a1c6a8e7bb560879983fa856696963
Chakrit Wichian <chakrit@oozou.com>
2012-06-27 15:33:11 +0700

    Add an example usage project.

---
+++ TabBarExample/OZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OZAppDelegate.h
+//  TabBarExample
+//
+//  Created by Chakrit Wichian on 6/27/12.
+//  Copyright (c) 2012 Oozou Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
